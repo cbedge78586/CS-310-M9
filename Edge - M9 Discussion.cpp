c555fbccf10f604377247bc48fc22ba016f0f9a6
Charles Edge <cbedge@my365.bellevue.edu>
2025-11-04 05:02:30 -0600

    Add files via upload

---
+++ Edge - M9 Discussion.cpp
@@ -0,0 +1,47 @@
+﻿// Charles Edge
+// CS310-T301
+// M9: Discussion
+
+#include <iostream>
+using namespace std;
+
+// This is the base class
+class Student {
+public:
+    void makeSound() {
+        cout << "The student says hi." << endl;
+    }
+};
+
+// This is the child class 1
+class SchoolClass : public Student {
+public:
+    void study() {
+
+// Blank line and print output to screen
+        cout << "\n";
+        cout << "The class is studying the lecture material." << endl;
+    }
+};
+
+// This is the child class 2
+class GroupProject : public studn3t {  
+public:
+    void collaborate() {
+        cout << "The students are working on this project together." << endl;
+    }
+};
+
+int main() {
+    SchoolClass myClass;
+    myClass.makeSound();
+    myClass.study();
+
+    GroupProject team;
+    team.collaborate();
+
+  
+    myClass.plygame();  
+
+    return 0;
+}
